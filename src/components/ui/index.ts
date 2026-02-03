a38a15db17afd7edf3ff5e21b5d422ace6670039
Mark388-Dad <tonnyodundo388@gmail.com>
2026-02-03 15:55:33 +0300

    Create index.ts
    Signed-off-by: Mark388-Dad <tonnyodundo388@gmail.com>

---
+++ src/components/ui/index.ts
@@ -0,0 +1,17 @@
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Label } from './Label';
+export { default as Select } from './Select';
+export { default as SelectTrigger } from './SelectTrigger';
+export { default as SelectValue } from './SelectValue';
+export { default as SelectContent } from './SelectContent';
+export { default as SelectItem } from './SelectItem';
+export { default as Card } from './Card';
+export { default as CardHeader } from './CardHeader';
+export { default as CardTitle } from './CardTitle';
+export { default as CardDescription } from './CardDescription';
+export { default as CardContent } from './CardContent';
+export { default as Tabs } from './Tabs';
+export { default as TabsList } from './TabsList';
+export { default as TabsTrigger } from './TabsTrigger';
+export { default as TabsContent } from './TabsContent';
