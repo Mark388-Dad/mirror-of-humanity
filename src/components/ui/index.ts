9cb0c4d7403813bc2745291d1cc4fcda579de970
Mark388-Dad <tonnyodundo388@gmail.com>
2026-03-18 09:39:37 +0300

    Refactor UI component exports to kebab-case
    Updated exports to use kebab-case filenames for consistency.
    
    Signed-off-by: Mark388-Dad <tonnyodundo388@gmail.com>

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,22 +1,21 @@
 // src/components/ui/index.ts
-// Barrel export for all UI components using lowercase filenames
-
 export { default as Button } from "./button";
 export { default as Input } from "./input";
 export { default as Label } from "./label";
 export { default as Select } from "./select";
-export { default as SelectTrigger } from "./selecttrigger";
-export { default as SelectValue } from "./selectvalue";
-export { default as SelectContent } from "./selectcontent";
-export { default as SelectItem } from "./selectitem";
+export { default as SelectTrigger } from "./select-trigger";
+export { default as SelectValue } from "./select-value";
+export { default as SelectContent } from "./select-content";
+export { default as SelectItem } from "./select-item";
 export { default as Card } from "./card";
-export { default as CardHeader } from "./cardheader";
-export { default as CardTitle } from "./cardtitle";
-export { default as CardContent } from "./cardcontent";
-export { default as CardDescription } from "./carddescription";
+export { default as CardHeader } from "./card-header";
+export { default as CardTitle } from "./card-title";
+export { default as CardContent } from "./card-content";
+export { default as CardDescription } from "./card-description";
 export { default as Tabs } from "./tabs";
-export { default as TabsList } from "./tabslist";
-export { default as TabsTrigger } from "./tabstrigger";
-export { default as TabsContent } from "./tabscontent";
+export { default as TabsList } from "./tabs-list";
+export { default as TabsTrigger } from "./tabs-trigger";
+export { default as TabsContent } from "./tabs-content";
 export { default as Alert } from "./alert";
-export { default as AlertDescription } from "./alertdescription";
+export { default as AlertDescription } from "./alert-description";
+// ...add the rest using exact kebab-case filenames
