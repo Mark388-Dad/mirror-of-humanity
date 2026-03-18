ed05e57a60df58158706d2f83d98080fdd44e757
Mark388-Dad <tonnyodundo388@gmail.com>
2026-03-18 09:26:30 +0300

    Refactor UI component exports to default syntax
    Updated the export statements for UI components to use default exports.
    
    Signed-off-by: Mark388-Dad <tonnyodundo388@gmail.com>

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,18 +1,22 @@
 // src/components/ui/index.ts
-export * from './Button'
-export * from './Input'
-export * from './Label'
-export * from './Select'
-export * from './SelectContent'
-export * from './SelectItem'
-export * from './SelectTrigger'
-export * from './SelectValue'
-export * from './Card'
-export * from './CardContent'
-export * from './CardHeader'
-export * from './CardTitle'
-export * from './CardDescription'
-export * from './Tabs'
-export * from './TabsContent'
-export * from './TabsList'
-export * from './TabsTrigger'
+// Clean re-exports for all UI components
+
+export { default as Button } from "./Button";
+export { default as Input } from "./Input";
+export { default as Label } from "./Label";
+export { default as Select } from "./Select";
+export { default as SelectTrigger } from "./SelectTrigger";
+export { default as SelectContent } from "./SelectContent";
+export { default as SelectItem } from "./SelectItem";
+export { default as SelectValue } from "./SelectValue";
+export { default as Card } from "./Card";
+export { default as CardHeader } from "./CardHeader";
+export { default as CardContent } from "./CardContent";
+export { default as CardTitle } from "./CardTitle";
+export { default as CardDescription } from "./CardDescription";
+export { default as Tabs } from "./Tabs";
+export { default as TabsList } from "./TabsList";
+export { default as TabsTrigger } from "./TabsTrigger";
+export { default as TabsContent } from "./TabsContent";
+export { default as Alert } from "./Alert";
+export { default as AlertDescription } from "./AlertDescription";
