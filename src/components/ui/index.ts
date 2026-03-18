862078ef85bb08b010bea6f477eb2f5f8fe8ce47
Mark388-Dad <tonnyodundo388@gmail.com>
2026-03-18 09:34:36 +0300

    Refactor UI component exports to lowercase
    Updated component exports to use lowercase filenames.
    
    Signed-off-by: Mark388-Dad <tonnyodundo388@gmail.com>

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,22 +1,22 @@
 // src/components/ui/index.ts
-// Clean re-exports for all UI components
+// Barrel export for all UI components (lowercase filenames)
 
-export { default as Button } from "./Button";
-export { default as Input } from "./Input";
-export { default as Label } from "./Label";
-export { default as Select } from "./Select";
-export { default as SelectTrigger } from "./SelectTrigger";
-export { default as SelectContent } from "./SelectContent";
-export { default as SelectItem } from "./SelectItem";
-export { default as SelectValue } from "./SelectValue";
-export { default as Card } from "./Card";
-export { default as CardHeader } from "./CardHeader";
-export { default as CardContent } from "./CardContent";
-export { default as CardTitle } from "./CardTitle";
-export { default as CardDescription } from "./CardDescription";
-export { default as Tabs } from "./Tabs";
-export { default as TabsList } from "./TabsList";
-export { default as TabsTrigger } from "./TabsTrigger";
-export { default as TabsContent } from "./TabsContent";
-export { default as Alert } from "./Alert";
-export { default as AlertDescription } from "./AlertDescription";
+export { default as Button } from "./button";
+export { default as Input } from "./input";
+export { default as Label } from "./label";
+export { default as Select } from "./select";
+export { default as SelectTrigger } from "./selectTrigger";
+export { default as SelectValue } from "./selectValue";
+export { default as SelectContent } from "./selectContent";
+export { default as SelectItem } from "./selectItem";
+export { default as Card } from "./card";
+export { default as CardHeader } from "./cardHeader";
+export { default as CardTitle } from "./cardTitle";
+export { default as CardContent } from "./cardContent";
+export { default as CardDescription } from "./cardDescription";
+export { default as Tabs } from "./tabs";
+export { default as TabsList } from "./tabsList";
+export { default as TabsTrigger } from "./tabsTrigger";
+export { default as TabsContent } from "./tabsContent";
+export { default as Alert } from "./alert";
+export { default as AlertDescription } from "./alertDescription";
