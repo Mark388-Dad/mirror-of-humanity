9d99b15898fdf6f9e549d4748b8410e6a5b8c83f
Mark388-Dad <tonnyodundo388@gmail.com>
2026-03-18 09:37:06 +0300

    Update index.ts
    Signed-off-by: Mark388-Dad <tonnyodundo388@gmail.com>

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,22 +1,22 @@
 // src/components/ui/index.ts
-// Barrel export for all UI components (lowercase filenames)
+// Barrel export for all UI components using lowercase filenames
 
 export { default as Button } from "./button";
 export { default as Input } from "./input";
 export { default as Label } from "./label";
 export { default as Select } from "./select";
-export { default as SelectTrigger } from "./selectTrigger";
-export { default as SelectValue } from "./selectValue";
-export { default as SelectContent } from "./selectContent";
-export { default as SelectItem } from "./selectItem";
+export { default as SelectTrigger } from "./selecttrigger";
+export { default as SelectValue } from "./selectvalue";
+export { default as SelectContent } from "./selectcontent";
+export { default as SelectItem } from "./selectitem";
 export { default as Card } from "./card";
-export { default as CardHeader } from "./cardHeader";
-export { default as CardTitle } from "./cardTitle";
-export { default as CardContent } from "./cardContent";
-export { default as CardDescription } from "./cardDescription";
+export { default as CardHeader } from "./cardheader";
+export { default as CardTitle } from "./cardtitle";
+export { default as CardContent } from "./cardcontent";
+export { default as CardDescription } from "./carddescription";
 export { default as Tabs } from "./tabs";
-export { default as TabsList } from "./tabsList";
-export { default as TabsTrigger } from "./tabsTrigger";
-export { default as TabsContent } from "./tabsContent";
+export { default as TabsList } from "./tabslist";
+export { default as TabsTrigger } from "./tabstrigger";
+export { default as TabsContent } from "./tabscontent";
 export { default as Alert } from "./alert";
-export { default as AlertDescription } from "./alertDescription";
+export { default as AlertDescription } from "./alertdescription";
